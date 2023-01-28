0eac46f5dfd552f0e4f8d0a655bc93abc34cc6c3
Slava <ipvorobyev@mail.ru>
2023-01-28 14:42:48 +0300

    django_2

--- django_2/main/models.py
+++ django_2/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField('Наименование', max_length=100)
+    description = models.TextField('Краткое описание')
+    text = models.TextField('Текст статьи')
+    data = models.DateTimeField('Дата публикации')
+
+    def __str__(self):
+        return self.title
